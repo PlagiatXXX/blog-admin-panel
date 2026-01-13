7ec67ca24414b2dc04e77dc99c4201952145d05b
PlagiatXXX <fedorvasin68@gmail.com>
2026-01-13 14:18:11 +0300

    Fix deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://my-json-server.typicode.com/PlagiatXXX/blog-admin-panel';
 
 // Типизируем данные, которые мы ожидаем от API
 export interface Post {
